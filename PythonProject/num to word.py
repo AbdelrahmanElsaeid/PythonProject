0c4a8a296471776a3a21a92653dcf6fb7d15951c
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2022-11-06 22:27:34 +0200

    create repo

---
+++ PythonProject/num to word.py
@@ -0,0 +1,11 @@
+# pip install num2words
+
+from num2words import num2words
+  
+
+print(num2words(36))
+print(num2words(36, to = 'ordinal'))
+print(num2words(36, to = 'ordinal_num'))
+print(num2words(36, to = 'year'))
+print(num2words(36, to = 'currency'))
+  
