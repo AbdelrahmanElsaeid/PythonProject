7954b30714461a937a6e8de589ec93957c3cd32a
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2022-11-08 18:23:49 +0200

    project

---
+++ PythonProject/Count the frequency of each unique element in a string.py
@@ -0,0 +1,16 @@
+
+def freq(str):
+
+	str_list = str.split()
+
+	unique_words = set(str_list)
+	
+	for words in unique_words :
+		print('Frequency of ', words , 'is :', str_list.count(words))
+
+	
+str ='ahmed abdo Ali abdo abdo abdo ahmed mido mido mido'
+	
+
+freq(str)
+  
