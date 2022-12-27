fd16f5aef339554f7f2d14fe1b949ac67fa1f9dd
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2022-12-27 14:34:31 +0200

    Update Google Image Downloader.py

--- PythonProject/Google Image Downloader.py
+++ PythonProject/Google Image Downloader.py
@@ -17,10 +17,10 @@ print(paths)
 
 
 
-# keywors: The item we want to search
-# limit: The max number we download
-# print_urls: print the website link or not
+#   keywors: The item we want to search
+#   limit: The max number we download
+#   print_urls: print the website link or not
 
-# And we use “downlaod()” function to start it. The output of program will show you the picture is download success or damage.
+#   And we use “downlaod()” function to start it. The output of program will show you the picture is download success or damage.
 
-# The pictures we download will put in the a new folder (The name is your “keyword”) it created automatically in current folder.
+#   The pictures we download will put in the a new folder (The name is your “keyword”) it created automatically in current folder.
