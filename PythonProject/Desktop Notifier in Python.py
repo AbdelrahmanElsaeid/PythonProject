00fbeea242376010f6bcc007377f4ee9b2fa43c9
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2022-12-28 22:39:19 +0200

    notifications

---
+++ PythonProject/Desktop Notifier in Python.py
@@ -0,0 +1,16 @@
+# installing the requests package  
+#           pip install requests  
+# installing the plyer package  
+#           pip install plyer
+
+from plyer import notification
+notification_title = 'GREETINGS FROM JAVATPOINT!'
+notification_message = 'Thank you for reading. Have a Good Day.'
+
+notification.notify(
+title = notification_title,
+message = notification_message,
+app_icon = None,
+timeout = 10,
+toast = False
+)
